ab94075c32b6ea21dc3e68786a98a58c230a7dcd
Reza Maliki Akbar <rezamaliki.akbar@gmail.com>
2021-02-18 03:42:50 +0700

    UI task 2

--- flutter_paycard_week1_task2/lib/main.dart
+++ flutter_paycard_week1_task2/lib/main.dart
@@ -5,16 +5,19 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Payment Card Demo',
       theme: ThemeData(
-       
+        primaryColor: Colors.purple,
         primarySwatch: Colors.purple,
-      ),
+        inputDecorationTheme: const InputDecorationTheme(
+             labelStyle: TextStyle(color: Colors.grey),
+             hintStyle: TextStyle(color: Colors.black),
+           ),
+        ),
       home: MyHomePage(title: 'Payment Card'),
     );
   }
@@ -28,22 +31,119 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController cardnameController = TextEditingController();
+  TextEditingController cardnumberController = TextEditingController();
+  TextEditingController cvvController = TextEditingController();
+  TextEditingController expdateController = TextEditingController();
   
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
+    return new Scaffold(
       appBar: AppBar(
     
         title: Center(child: Text(widget.title)),
       ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          //mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-           
+
+             Container(
+                  height: 100,
+                  padding: EdgeInsets.all(20),
+                  child: TextField(
+                    controller: cardnameController,
+                    decoration: InputDecoration(
+                      icon: const Icon(
+                      Icons.person,
+                      color: Colors.grey,
+                      size: 40,
+                      ),
+                      labelText: 'Card Name',                     
+                    ),
+                  ),
+                ),
+
+                
+                Container(
+                  height: 80,
+                  padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: TextField(
+                    controller: cardnumberController,
+                    decoration: InputDecoration(   
+                      icon: const Icon(
+                      Icons.payment,
+                      color: Colors.grey,
+                      size: 40,
+                      ),                   
+                      labelText: 'Number',                  
+                    ),
+
+                    ),
+                  ),
+
+                Container(
+                  height: 80,
+                  padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: TextField(
+                    controller: cvvController,
+                    decoration: InputDecoration(   
+                      icon: const Icon(
+                      Icons.payments,
+                      color: Colors.grey,
+                      size: 40,
+                      ),                   
+                      labelText: 'CVV',                  
+                    ),
+
+                    ),
+                  ), 
+
+               Container(
+                  height: 80,
+                  padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: TextField(
+                    controller: expdateController,
+                    decoration: InputDecoration(   
+                      icon: const Icon(
+                      Icons.calendar_today_rounded,
+                      color: Colors.grey,
+                      size: 40,
+                      ),                   
+                      labelText: 'Expiry Date',                      
+                    ),
+
+                    ),
+                  ),  
+
+                  
+              
+                  Container(        
+                    width: 40,
+                    height: 80,
+                    padding: EdgeInsets.fromLTRB(90, 10, 90, 10),
+                    child: ElevatedButton(                   
+                      style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            onPrimary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                            ),
+                      ),
+                      child: Text('Pay', style: TextStyle(fontSize: 18, color: Colors.white)),
+                      onPressed: () {
+                         print(cardnameController.text);
+                        print(cardnumberController.text);
+                        print(cvvController.text);
+                        print(expdateController.text);
+                      }, 
+                    )
+                    
+                    ),
+                  
+
           ],
         ),
       ),
