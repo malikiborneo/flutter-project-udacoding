648a47aab847347cff3c78e6e524b92045e515af
Reza Maliki Akbar <rezamaliki.akbar@gmail.com>
2021-03-07 22:55:14 +0700

    yo

---
+++ flutter_knowternak_week3_task4/lib/homepage.dart
@@ -0,0 +1,29 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_knowternak_week3_task4/main.dart';
+import '';
+import '';
+import '';
+
+void main() = > runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      title: 'KnowTernak',
+      theme: ThemeData(primarySwatch: Colors.blue,)
+      ,
+      home: PageHome(),
+      );
+
+  }
+}
+
+class PageHome extends StatelessWidget {
+  @override 
+  
+
+}
