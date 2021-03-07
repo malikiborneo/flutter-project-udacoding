5a87284bc5bb23b94c8d310a01ffda299d069ba0
Reza Maliki Akbar <rezamaliki.akbar@gmail.com>
2021-03-07 21:47:33 +0700

    banyak

--- flutter_knowternak_week3_task4/lib/main.dart
+++ flutter_knowternak_week3_task4/lib/main.dart
@@ -1,3 +1,11 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ flutter_knowternak_week3_task4/php/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+
+define('HOST', 'https://malikiborneo.nasiwebhost.com');
+define('USER', 'malikiborneo');
+define('PASS', 'Malikigantengs7981');
+define('DB', 'malikiborneo');
+
+$con = mysqli_connect(HOST, USER, PASS, DB) or die("Can't connect");
+
+?>
